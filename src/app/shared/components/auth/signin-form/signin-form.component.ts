2586c5433a3caa1b147427a4fe958e488f71c849
bannaphol <bannaphol@gmail.com>
2026-04-19 22:34:37 +0700

    feat: add auth sign-in

--- src/app/shared/components/auth/signin-form/signin-form.component.ts
+++ src/app/shared/components/auth/signin-form/signin-form.component.ts
@@ -1,19 +1,19 @@
 
 import { Component } from '@angular/core';
 import { LabelComponent } from '../../form/label/label.component';
-import { CheckboxComponent } from '../../form/input/checkbox.component';
 import { ButtonComponent } from '../../ui/button/button.component';
 import { InputFieldComponent } from '../../form/input/input-field.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/api/auth.service';
 
 @Component({
   selector: 'app-signin-form',
   imports: [
     LabelComponent,
-    CheckboxComponent,
     ButtonComponent,
     InputFieldComponent,
+    ReactiveFormsModule,
     RouterModule,
     FormsModule
 ],
@@ -27,6 +27,51 @@ export class SigninFormComponent {
 
   email = '';
   password = '';
+
+  protected errorSignIn: boolean = false;
+
+  constructor(
+    private readonly router: Router,
+    private readonly _authService: AuthService
+  ) {}
+
+  signInForm = new FormGroup({
+    email: new FormControl('',{nonNullable: true,validators: [Validators.required, Validators.email]}),
+    password: new FormControl('',{nonNullable: true,validators: [Validators.required]}),
+  });
+
+  get emailCtrl() {
+    return this.signInForm.controls.email;
+  }
+
+  get passwordCtrl() {
+    return this.signInForm.controls.password;
+  }
+
+  get emailInvalid() {
+    return this.emailCtrl.invalid && (this.emailCtrl.touched || this.emailCtrl.dirty);
+  }
+
+  get passwordInvalid() {
+    return this.passwordCtrl.invalid && (this.passwordCtrl.touched || this.passwordCtrl.dirty);
+  }
+
+  async onSubmit() {
+    if (this.signInForm.valid) {
+      const body = this.signInForm.getRawValue();
+      const result = await this._authService.signIn({ email: body.email, password: body.password });
+      
+      if (result?.status === 200) {
+        console.log("Sign-in successful:", result);
+        this.router.navigate(['']);
+      }
+
+    } else {
+      this.signInForm.markAllAsTouched();
+      console.log("Form is invalid:", this.signInForm.valid);
+      return;
+    }
+  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
---
+++ src/app/shared/services/interfaces/auth.interface.ts
@@ -0,0 +1,14 @@
+export interface SignInPayload {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+    status: number;
+    resultData:[{
+        accessToken: string;
+    }];
+    message: string;
+    console: string;
+    statusCode: number;
+}
