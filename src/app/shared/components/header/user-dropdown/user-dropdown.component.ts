16aca94cde846602a86af7e5c91c7fe94108c118
bannaphol <bannaphol@gmail.com>
2026-04-22 01:24:23 +0700

    feat: add signout

--- src/app/shared/components/header/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/components/header/user-dropdown/user-dropdown.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { DropdownComponent } from '../../ui/dropdown/dropdown.component';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { DropdownItemTwoComponent } from '../../ui/dropdown/dropdown-item/dropdown-item.component-two';
+import { Component } from "@angular/core";
+import { DropdownComponent } from "../../ui/dropdown/dropdown.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { DropdownItemTwoComponent } from "../../ui/dropdown/dropdown-item/dropdown-item.component-two";
+import { TokenStorageService } from "../../../services/api/common/token-storage-service";
 
 @Component({
-  selector: 'app-user-dropdown',
-  templateUrl: './user-dropdown.component.html',
-  imports:[CommonModule,RouterModule,DropdownComponent,DropdownItemTwoComponent]
+  selector: "app-user-dropdown",
+  templateUrl: "./user-dropdown.component.html",
+  imports: [
+    CommonModule,
+    RouterModule,
+    DropdownComponent,
+    DropdownItemTwoComponent,
+  ],
 })
 export class UserDropdownComponent {
   isOpen = false;
+
+  constructor(private readonly _tokenStrorage: TokenStorageService) {}
+
+  signOut(){
+    this._tokenStrorage.clearToken();
+  }
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
