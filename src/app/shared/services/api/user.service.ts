addc1a2dee327b77fc7d54a45a991e41bacfdb77
bannaphol <bannaphol@gmail.com>
2026-04-23 14:23:38 +0700

    feat: add list user for admin

--- src/app/shared/services/api/user.service.ts
+++ src/app/shared/services/api/user.service.ts
@@ -2,13 +2,16 @@ import Axios from "./axios";
 import { Injectable } from "@angular/core";
 import type { AxiosResponse } from "axios";
 
-import { UserPayload, UserResponse } from "../../services/interfaces/user.interface";
+import {
+  AllUserResponse,
+  UserPayload,
+  UserResponse,
+} from "../../services/interfaces/user.interface";
 
 @Injectable({
   providedIn: "root",
 })
 export class UserService {
-
   // get user profile
   async getUserProfile(): Promise<UserResponse | null> {
     try {
@@ -23,10 +26,25 @@ export class UserService {
   }
 
   // get user list
-  async load(): Promise<UserResponse | null> {
+  async load(
+    page: number = 1,
+    pageSize: number = 6,
+    fullName?: string,
+    email?: string,
+  ): Promise<AllUserResponse | null> {
     try {
-      const response: AxiosResponse<UserResponse> =
-        await Axios().get("/users");
+      const params: Record<string, any> = {
+        page,
+        page_size: pageSize,
+      };
+
+      if (fullName) params["full_name"] = fullName;
+      if (email) params["email"] = email;
+
+      const response: AxiosResponse<AllUserResponse> = await Axios().get(
+        "/users",
+        { params },
+      );
 
       return response.data;
     } catch (error: any) {
@@ -35,30 +53,33 @@ export class UserService {
     }
   }
 
-  async patchUserProfile(userId: string ,payload: UserPayload): Promise<UserResponse | null> {
-    try{
-      const response: AxiosResponse<UserResponse> =
-      await Axios().patch(`/users/${userId}`, payload);
+  async patchUserProfile(
+    userId: string,
+    payload: UserPayload,
+  ): Promise<UserResponse | null> {
+    try {
+      const response: AxiosResponse<UserResponse> = await Axios().patch(
+        `/users/${userId}`,
+        payload,
+      );
 
       return response.data;
-    }catch (error: any){
-      console.log('errorrrr: ',error)
-      return error?.response?.data ?? null ;
+    } catch (error: any) {
+      console.log("errorrrr: ", error);
+      return error?.response?.data ?? null;
     }
-
   }
 
   async deleteUser(userId: string): Promise<UserResponse | null> {
-    try{
-      const response: AxiosResponse<UserResponse> =
-      await Axios().delete(`/users/${userId}`);
+    try {
+      const response: AxiosResponse<UserResponse> = await Axios().delete(
+        `/users/${userId}`,
+      );
 
       return response.data;
-    }catch (error: any){
-      console.log('errorrrr: ',error)
-      return error?.response?.data ?? null ;
+    } catch (error: any) {
+      console.log("errorrrr: ", error);
+      return error?.response?.data ?? null;
     }
-
   }
-
 }
